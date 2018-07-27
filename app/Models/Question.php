c1e8d9ba4aa8d9eea0bd8a4542f576b02ac08d65
Matt Larson <matt.a.larson@gmail.com>
2018-07-27 14:49:54 -0500

    created relationships between models

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Reply;
+use App\Models\Category;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Question;
+use App\Models\User;
+use App\Models\Like;
 
 class Reply extends Model
 {
-    //
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function like()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
