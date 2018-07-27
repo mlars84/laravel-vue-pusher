5d6354b61394bcad37a16a422313076921743e8f
Matt Larson <matt.a.larson@gmail.com>
2018-07-27 15:17:28 -0500

    seeded DB

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -1,9 +1,17 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Models\Question;
+use App\Models\User;
 
 $factory->define(App\Models\Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function () {
+            return Question::all()->random();
+        },
+        'user_id' => function () {
+            return User::all()->random();
+        }
     ];
 });
